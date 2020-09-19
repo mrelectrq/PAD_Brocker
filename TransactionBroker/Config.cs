8985281cfc5bc0d5008e2c46411893fe67fa0ecb
mrelectrq <tiora100bass@gmail.com>
2020-09-19 22:47:21 +0300

    Added configuration for Storage

---
+++ TransactionBroker/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransactionBroker
+{
+    public class Config
+    {
+        public static readonly string STORAGE_PATH = "D:\\Univer\\ANUL4\\PAD\\Laborator_1\\Storage";
+        public static readonly string STORAGE_NAME = "transactions.txt";
+    }
+}
